632a0f2e6bb58a122683547460614e437229f106
Allen <Allenpmraz@gmail.com>
2026-03-29 18:38:00 -0400

    Added Users.kt and worked on DirectoryApiService.kt some more.

---
+++ app/src/main/java/com/example/user_direcory_application/model/Users.kt
@@ -0,0 +1,9 @@
+package com.example.user_direcory_application.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Users(
+    val id: String,
+    val firstName: String
+)
--- app/src/main/java/com/example/user_direcory_application/network/DirectoryApiService.kt
+++ app/src/main/java/com/example/user_direcory_application/network/DirectoryApiService.kt
@@ -1,4 +1,26 @@
 package com.example.user_direcory_application.network
 
+
+import com.example.user_direcory_application.model.Users
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+
 private const val BASE_URL =
     "https://dummyjson.com/users?limit=20"
+
+private val retrofit = Retrofit.Builder().addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+interface DirectoryApiService {
+        @GET("users")
+        suspend fun getUsers(): List<Users>
+}
+
+object UserApi {
+    val retrofitService: DirectoryApiService by lazy {
+        retrofit.create(DirectoryApiService::class.java)
+    }
+}
